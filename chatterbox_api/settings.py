40df491f390cb46f10f9c2a1526b026b69dc5d57
FintanMi <>
2023-11-14 23:01:15 +0000

    added comment app with generics for list & detail views

--- chatterbox_api/settings.py
+++ chatterbox_api/settings.py
@@ -50,9 +50,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
-    
-    'posts',
+
     'profiles',
+    'posts',
+    'comments',
 ]
 
 MIDDLEWARE = [
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTest(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='finn', password='pass')
+
+    def test_can_list_posts(self):
+        finn = User.objects.get(username='finn')
+        Post.objects.create(owner=finn, title='a title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
