f75c85a2f0ba0d168d4606b1e7aeeac543036d80
FintanMi <>
2023-11-15 15:05:47 +0000

    annotated the profile & community views queryset

--- community/serializers.py
+++ community/serializers.py
@@ -5,6 +5,8 @@ from .models import Community
 class CommunitySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    profiles_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -12,4 +14,5 @@ class CommunitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Community
-        fields = '__all__'
+        fields = ['id', 'owner', 'name', 'description', 'users', 'comment',
+            'like', 'created_at', 'updated_at', 'posts_count', 'profiles_count']
--- community/views.py
+++ community/views.py
@@ -1,12 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Community
 from .serializers import CommunitySerializer
 from chatterbox_api.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 
 
 class CommunityList(generics.ListAPIView):
-    queryset = Community.objects.all()
+    queryset = Community.objects.annotate(
+        profiles_count = Count('owner__profile', distinct=True),
+        posts_count = Count('owner__post', distinct=True)
+    ).order_by('-created_at')
     serializer_class = CommunitySerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'profiles_count',
+        'posts_count',
+    ]
 
 
 class CommunityDetail(generics.RetrieveUpdateAPIView):
