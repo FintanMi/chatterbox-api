138205cef4268e954a3a8db72aabc29c392ddfb1
FintanMi <>
2023-11-21 17:48:37 +0000

    infinite scroll added & search functionality

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -10,16 +10,20 @@ import styles from "../../styles/Home.module.css";
 import { axiosReq } from '../../api/axiosDefault';
 import PostContent from '../posts/PostContent';
 import Asset from '../../components/Asset';
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from '../../utils/utils';
 
 function Home({ message, filter = '' }) {
     const [content, setContent] = useState({ results: [] });
     const [loaded, setLoaded] = useState(false);
     const { pathname } = useLocation();
 
+    const [searchFor, setSearchFor] = useState('');
+
     useEffect(() => {
         const fetchContent = async () => {
             try {
-                const { data } = await axiosReq.get(`/posts/?${filter}`);
+                const { data } = await axiosReq.get(`/posts/?${filter}search=${searchFor}`);
                 setContent(data);
                 setLoaded(true);
             } catch (err) {
@@ -27,8 +31,13 @@ function Home({ message, filter = '' }) {
             }
         };
         setLoaded(false);
-        fetchContent();
-    }, [filter, pathname]);
+        const timer = setTimeout(() => {
+            fetchContent();
+        }, 500);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [filter, searchFor, pathname]);
 
     return (
         <Row className="h-100">
@@ -37,16 +46,34 @@ function Home({ message, filter = '' }) {
             </Col>
             <Col className="py-2 p-0 p-lg-2" lg={6}>
                 <p>Popular profiles mobile</p>
+                <i className={`fas fa-search ${styles.SearchIcon}`} />
+                <Form className={styles.Searchbar} onSubmit={(e) => e.preventDefault()}>
+                    <Form.Control
+                        value={searchFor}
+                        onChange={(e) => setSearchFor(e.target.value)}
+                        type='text'
+                        placeholder='Search'
+                        className='mr-sm-2'
+                    >
+                    </Form.Control>
+                </Form>
                 {loaded ? (
                     <>
-                        {content.results.length
-                            ?
-                            content.results.map((contents) => (
-                                <PostContent key={contents.id} {...contents} setPosts={setContent} />
-                            ))
-                            : (<Container className={appStyles.Content}>
-                                <Asset message={message} />
-                            </Container>)}
+                        {content.results.length ? (
+                            <InfiniteScroll
+                                children={
+                                    content.results.map((contents) => (
+                                        <PostContent key={contents.id} {...contents} setPosts={setContent} />
+                                    ))
+                                }
+                                dataLength={content.results.length}
+                                loader={<Asset spinner />}
+                                hasMore={!!content.next}
+                                next={() => fetchMoreData(content, setContent)}
+                            />
+                        ) : (<Container className={appStyles.Content}>
+                            <Asset message={message} />
+                        </Container>)}
                     </>
                 ) : (
                     <Container className={appStyles.Content}>
